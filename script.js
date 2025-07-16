74fba98d5d97fd58c56b2d374f59615f70bdbe77
DevSamuelJ <samuel701silva@gmail.com>
2025-07-16 15:22:41 -0300

    Corrigindo algumas falhas e construindo o POST

--- script.js
+++ script.js
@@ -8,26 +8,29 @@ async function tarefas() {
     tarefas.forEach(tarefa => {
         let listaTarefas = document.querySelector("#listaTarefas")
         listaTarefas.innerHTML += `<li>${tarefa.tarefa}</li>`
-        
+        if (listaTarefas.textContent != ""){
+            let semTarefas = document.querySelector("#semTarefas")
+            semTarefas.style.display = "none"
+            
+        }
     });
 }
 
 const adicionarTarefa =  document.querySelector(".botao-adicionar")
 
 adicionarTarefa.onclick = function adicionarTarefa() {
+    let novatarefa  = document.querySelector("#novatarefa").value
+    let envio = {
+        tarefa: novatarefa,
+        status: false,
+        id: 8,
+    }
+    
     const url = "https://68771b77dba809d901edf03c.mockapi.io/api/Tarefas"
     fetch(url),{
-        method: "POST",
+        method: 'POST',
         headers: {'Content-Type': 'aplication/json'},
-        body: JSON.stringify(url)}
-
-
-
-
-
-
-
+        body: JSON.stringify(envio)}
 }
 
-// let novatarefa  = document.querySelector("#novatarefa").value
 tarefas()
