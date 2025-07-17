4ddf373b5f51bccf2b682106eedd3c97665d4bc0
DevSamuelJ <samuel701silva@gmail.com>
2025-07-17 01:29:35 -0300

    Tentando solucionar o problema do POST

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
-
-
 async function tarefas() {
-    const url = "https://68771b77dba809d901edf03c.mockapi.io/api/Tarefas"
-    tarefas = await fetch(url)
-    tarefas =  await tarefas.json()
-    console.log(tarefas)
-    tarefas.forEach(tarefa => {
-        let listaTarefas = document.querySelector("#listaTarefas")
-        listaTarefas.innerHTML += `<li>${tarefa.tarefa}</li>`
-        if (listaTarefas.textContent != ""){
-            let semTarefas = document.querySelector("#semTarefas")
-            semTarefas.style.display = "none"
-            
-        }
+    const url = "https://68771b77dba809d901edf03c.mockapi.io/api/Tarefas";
+    let resposta = await fetch(url); 
+    let dados = await resposta.json();
+    console.log(dados);
+
+    let listaTarefas = document.querySelector("#listaTarefas");
+    listaTarefas.innerHTML = ""; 
+
+    dados.forEach(tarefa => {
+        listaTarefas.innerHTML += `<li>${tarefa.tarefa}</li>`;
     });
-}
 
-const adicionarTarefa =  document.querySelector(".botao-adicionar")
-
-adicionarTarefa.onclick = function adicionarTarefa() {
-    let novatarefa  = document.querySelector("#novatarefa").value
-    let envio = {
-        tarefa: novatarefa,
-        status: false,
-        id: 8,
+    if (listaTarefas.textContent != "") {
+        let semTarefas = document.querySelector("#semTarefas");
+        semTarefas.style.display = "none";
     }
-    
-    const url = "https://68771b77dba809d901edf03c.mockapi.io/api/Tarefas"
-    fetch(url),{
-        method: 'POST',
-        headers: {'Content-Type': 'aplication/json'},
-        body: JSON.stringify(envio)}
 }
 
-tarefas()
+const adicionarTarefa = document.querySelector(".botao-adicionar");
+
+adicionarTarefa.onclick = async function adicionarTarefa() {
+    let novaTarefa = document.querySelector("#novaTarefa");
+    let envio = {
+        tarefa: novaTarefa.value,
+        status: false
+    };
+
+    await fetch("https://68771b77dba809d901edf03c.mockapi.io/api/Tarefas", {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(envio)
+    });
+
+    tarefas(); 
+};
+
+tarefas(); 
