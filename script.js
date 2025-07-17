46d1864d57e3f9c443c5b45927d7574e824dc97a
devsamuelj <samuel701silva@gmail.com>
2025-07-17 18:57:32 -0300

    Corrigido o método POST com sucesso.

--- script.js
+++ script.js
@@ -18,6 +18,24 @@ async function tarefas() {
 }
 
 const adicionarTarefa = document.querySelector(".botao-adicionar");
+// adicionarTarefa.addEventListener("click",async (event) =>{
+//     event.preventDefault()
+//     let novaTarefa = document.querySelector("#novaTarefa");
+//     let envio = {
+//         tarefa: novaTarefa.value,
+//         status: false
+//     };
+    
+//     await fetch("https://68771b77dba809d901edf03c.mockapi.io/api/Tarefas", {
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify(envio)
+//     });
+    
+//     tarefas(); 
+
+// })
+
 
 adicionarTarefa.onclick = async function adicionarTarefa() {
     let novaTarefa = document.querySelector("#novaTarefa");
@@ -34,5 +52,7 @@ adicionarTarefa.onclick = async function adicionarTarefa() {
 
     tarefas(); 
 };
+
+
 
 tarefas(); 
